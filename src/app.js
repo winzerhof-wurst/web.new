36737d9ef908b83aff2bac3823ff5477b297d14b
Christoph Wurst <christoph@winzerhof-wurst.at>
2018-10-24 16:40:07 +0200

    Fix matomo tracking
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import BootstrapVue from 'bootstrap-vue'
 import Vue from 'vue'
+import VueMatomo from 'vue-matomo'
 
 import App from './App.vue'
 import { createRouter } from './router'
@@ -15,6 +16,16 @@ export function createApp(onServer) {
     const store = createStore(coreUrl)
 
     sync(store, router)
+
+    Vue.use(VueMatomo, {
+        host: 'https://piwik.wuc.me',
+        siteId: 2,
+        router,
+
+        // Changes the default .js and .php endpoint's filename
+        // Default: 'piwik'
+        trackerFileName: 'piwik'
+    });
 
     const app = new Vue({
         router,
