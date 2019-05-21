aa108b4e6be8fc8e2acc62122cc672160474a87f
Christoph Wurst <christoph@winzerhof-wurst.at>
2019-05-21 11:31:56 +0200

    Add sortensieger info
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/views/about/News.vue
+++ src/views/about/News.vue
@@ -3,6 +3,17 @@
         <h1>Aktuelles</h1>
 
         <ContentTile>
+            <h2>Retzer Weinwoche</h2>
+            <p>
+              Mit großer Freude dürfen wir uns heuer über gleich zwei Sortensieger bei der Retzer Weinwoche freuen. Unser H&P Weissweincuveé sowie unser Rosé-Merlot haben die Jury überzeugt und werden bei der Eröffnung am 20. Juni in Retz ausgezeichnet.
+Sie können die <router-link :to="{name: 'wines'}">Weine</router-link> schon jetzt direkt bei uns erwerben.
+            </p>
+            <img class="img-fluid"
+                     :src="require('../../images/201905_sortensieger.jpg')"
+                     alt="Sortensieger Winzerhof Wurst der Retzer Weinwoche 2019">
+        </ContentTile>
+
+        <ContentTile>
             <h2>Urlaub am Winzerhof</h2>
             <div class="embed-responsive embed-responsive-16by9">
               <iframe width="640" height="360" src="https://www.youtube.com/embed/O2i49NVaYNs" frameborder="0" allowfullscreen></iframe>
