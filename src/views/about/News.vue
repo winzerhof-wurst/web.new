7c7a43c7e13aa0904f1a17287523610488b7d06b
Christoph Wurst <christoph@winzerhof-wurst.at>
2019-09-30 11:20:35 +0200

    Add Weinlese post
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/views/about/News.vue
+++ src/views/about/News.vue
@@ -3,6 +3,16 @@
         <h1>Aktuelles</h1>
 
         <ContentTile>
+            <h2>Weinlese 2019</h2>
+            <p>
+              Die fünfte Jahreszeit hat begonnen und der Großteil der Trauben wurden bereits geerntet und verarbeitet. Das gesunde Traubenmaterial bietet eine optimale Grundlage für den neuen Jahrgang.
+            </p>
+             <img class="img-fluid"
+                     :src="require('../../images/201909_weinlese.jpg')"
+                     alt="Pollak Retzbacherhof">
+        </ContentTile>
+
+        <ContentTile>
             <h2>Urlaub am Winzerhof</h2>
             <div class="embed-responsive embed-responsive-16by9">
               <iframe width="640" height="360" src="https://www.youtube.com/embed/O2i49NVaYNs" frameborder="0" allowfullscreen></iframe>
