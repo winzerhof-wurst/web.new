8b2002c501668300e7897a7c7650acb378c9269b
Christoph Wurst <christoph@winzerhof-wurst.at>
2020-05-26 07:48:08 +0200

    Update Genießerzimmer description
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/views/rooms/Geniesserzimmer.vue
+++ src/views/rooms/Geniesserzimmer.vue
@@ -4,7 +4,7 @@
 
     <ContentTile>
       <h2>Genießerzimmer</h2>
-      <p>Das Genießerzimmer bietet Komfort auf höchstem Niveau. Das Zimmer verfügt neben einem separaten Wohnzimmer auch einen eigenen Balkon, um unsere Weine zu genießen. Egal zu welcher Jahreszeit können Sie hier zu zweit oder aber auch als Familie Ihren Urlaub verbringen. In diesem Zimmer befindet sich auch eine kleine Kochnische, die mit einem Wasserkocher, einer Kaffeemaschine, einem Cerankochfeld sowie einem Kühlschrank ausgestattet ist. Je nach Ihren Wünschen können wir Ihnen nach Absprache auch Kochgeschirr für kleine Speisen zur Verfügung stellen.</p>
+      <p>Das Genießerzimmer bietet Komfort auf höchstem Niveau. Das Zimmer verfügt neben einem separaten Wohnzimmer auch einen eigenen Balkon, um unsere Weine zu genießen. Egal zu welcher Jahreszeit können Sie hier zu zweit oder aber auch als Familie Ihren Urlaub verbringen.</p>
       <p>
         <img
           class="img-fluid center-block rounded"
