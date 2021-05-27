80ce7e1182cc44eb2befb8ab1deb1bb95ab544d5
Christoph Wurst <christoph@winzerhof-wurst.at>
2021-05-27 09:30:35 +0200

    Update news
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/views/about/News.vue
+++ src/views/about/News.vue
@@ -3,27 +3,11 @@
         <h1>Aktuelles</h1>
 
         <ContentTile>
-          <h2>Austrieb</h2>
-          <p>Durch die warmen Temperaturen und den Regenschauern in den letzten Tagen zeigen nun auch unsere Reben die ersten jungen Triebe und Blättern.</p>
+          <h2>Weintour Weinviertel</h2>
+          <p>Es ist soweit, das Weinviertel öffnet seine Türen. Wir und unsere Winzerkollegen der Weinstraße Weinviertel möchten Sie zur Weintour am 26. und 27. Juni einladen. Kommen Sie in unseren Hof und verkosten Sie in gemütlicher Atmosphäre unsere Weine.</p>
           <img class="img-fluid"
-                     :src="require('../../images/202105_austrieb.jpg')"
-                     alt="Austrieb der Weinreben am Winzerhof Wurst">
-        </ContentTile>
-
-        <ContentTile>
-          <h2>Jahrgang 2020</h2>
-          <p>Von vielen bereits sehnsüchtig erwartet - jetzt sind sie da. Neben den ersten  Frühlingsboten lässt sich nun auch der neue Jahrgang blicken. Unsere Grünen Veltliner sind abgefüllt und ab sofort bei uns erhältlich. In den kommenden Wochen werden weitere 2020er in die Flasche kommen. Das gesamte aktuelle Sortiment findet Ihr in unserer Weinkarte.</p>
-          <img class="img-fluid"
-                     :src="require('../../images/202102_dac.jpg')"
-                     alt="Weingarten Schneiden Winzerhof Wurst">
-        </ContentTile>
-
-        <ContentTile>
-          <h2>Weinviertel DAC Champion 2020</h2>
-          <p>Jedes Jahr werden Ende Juni im Zuge der Retzer Weinwoche die Weine der Region ausgekostet und die besten prämiert. Wir dürfen uns heuer über einen Sortensieger erfreuen. Unser Weinviertel DAC Hochsteiner 2019 belegt den 1.Platz in der Kategorie Weinviertel DAC und trägt damit den Namen <em>Weinviertel DAC Champion 2020</em>.</p>
-          <img class="img-fluid"
-                     :src="require('../../images/202006_dac_champion.jpg')"
-                     alt="Weinviertel DAC Champion 2020 Winzerhof Wurst">
+                     :src="require('../../images/202106_weintour.jpg')"
+                     alt="Weintour Weinviertel 2021">
         </ContentTile>
 
         <ContentTile>
